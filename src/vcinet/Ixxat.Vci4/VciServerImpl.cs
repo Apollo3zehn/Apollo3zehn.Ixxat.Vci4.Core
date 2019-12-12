76cffe4ff909e200768b417c28fa82651ce2d1de
Vincent Wilms <vwilms@outlook.com>
2019-12-12 16:43:24 +0100

    Initial commit

---
+++ src/vcinet/Ixxat.Vci4/VciServerImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ixxat.Vci4
+{
+	public class VciServerImpl
+	{
+		public static VciServerImpl Instance()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IVciDeviceManager DeviceManager
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public IMessageFactory MsgFactory
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
